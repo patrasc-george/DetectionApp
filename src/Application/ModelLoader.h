b5d52e410068a63425fdacefd348f244fee2909a
patrasc-george <georgedanielp14@gmail.com>
2023-05-07 19:31:20 +0300

    test

--- src/Application/ModelLoader.h
+++
@@ -1,9 +0,0 @@
-#include <ObjectDetection.h>
-
-class ModelLoader {
-public:
-	static void loadAll(QString jsonPath, QVector<Detector*>& vector);
-	static QVector<QString> getNames(QString jsonPath);
-	static bool getFromFileByName(Detector*&, QString name, QString path);
-	static bool getFromJsonObject(Detector*&, QJsonObject);
-};
