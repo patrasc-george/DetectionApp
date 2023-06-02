7e4f5394dd1f29b1e5b9db226a29783123ed6405
patrasc-george <georgedanielp14@gmail.com>
2023-06-02 11:28:17 +0300

    Added test files in cmake.

--- src/Application/main.cpp
+++ src/Application/main.cpp
@@ -10,23 +10,21 @@
 #include <QTimer>
 
 int main(int argc, char* argv[]) {
-    QApplication app(argc, argv);
+	QApplication app(argc, argv);
 
-    QTimer::singleShot(0, []() {
-        MainWindow window;
-        window.setObjectName("window");
+	MainWindow window;
+	window.setObjectName("window");
 
-        QFile file(":/assets/stylesheet.qss");
-        file.open(QFile::ReadOnly);
-        QString styleSheet = QLatin1String(file.readAll());
-        qApp->setStyleSheet(styleSheet);
+	QFile file(":/assets/stylesheet.qss");
+	file.open(QFile::ReadOnly);
+	QString styleSheet = QLatin1String(file.readAll());
+	qApp->setStyleSheet(styleSheet);
 
-        window.setWindowTitle("Detection App");
-        window.show();
-        window.setWindowIcon(QIcon(":/assets/camera_dark.png"));
-        window.menu->toggleCamera->setChecked(true); // camera is on
+	window.setWindowTitle("Detection App");
+	window.show();
+	window.setWindowIcon(QIcon(":/assets/camera_dark.png"));
+	window.menu->toggleCamera->setChecked(true); // camera is on
 
-        QCoreApplication::exit(0);
-        });
-    return app.exec();
+	QCoreApplication::exit(0);
+	return app.exec();
 }
