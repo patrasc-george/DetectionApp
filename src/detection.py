858ff0375653849d27349e12fa37ccf87197a913
patrasc-george <georgedanielp14@gmail.com>
2023-08-05 19:50:14 +0300

    The border color has changed.

--- src/detection.py
+++ src/detection.py
@@ -68,9 +68,9 @@ def YOLOv5(frame, net_name):
     for box in detected_objects:
         x1, y1, x2, y2, confidence, class_ = box
         x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
         class_name = class_names[class_]
-        cv2.putText(frame, class_name, (x1, y1 - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
+        cv2.putText(frame, class_name, (x1, y1 - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
 
     return frame
 
