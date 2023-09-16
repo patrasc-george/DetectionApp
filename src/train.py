07762f13f46a57408c89bff2d3d5dcac16f28067
patrasc-george <georgedanielp14@gmail.com>
2023-09-16 14:06:22 +0300

    Upload New File

---
+++ src/train.py
@@ -0,0 +1,69 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms, models
+from sklearn.model_selection import train_test_split
+
+transform = transforms.Compose([
+    transforms.Resize((150, 150)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+])
+
+data_dir = 'dataset'
+batch_size = 32
+dataset = datasets.ImageFolder(data_dir, transform=transform)
+class_names = dataset.classes
+
+train_dataset, test_dataset = train_test_split(dataset, test_size=0.2, random_state=42)
+train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=batch_size)
+
+model = models.resnet18(pretrained=True)
+num_classes = len(class_names)
+model.fc = nn.Linear(512, num_classes)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+num_epochs = 10
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device)
+
+for epoch in range(num_epochs):
+    model.train()
+    running_loss = 0.0
+    for inputs, labels in train_loader:
+        inputs, labels = inputs.to(device), labels.to(device)
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.item()
+    print(f"Epoch {epoch + 1}/{num_epochs}, Loss: {running_loss / len(train_loader)}")
+
+model.eval()
+correct_predictions = 0
+total_samples = 0
+
+with torch.no_grad():
+    for inputs, labels in test_loader:
+        inputs, labels = inputs.to(device), labels.to(device)
+        outputs = model(inputs)
+        _, predictions = torch.max(outputs, 1)
+        correct_predictions += (predictions == labels).sum()
+        total_samples += predictions.size(0)
+
+test_accuracy = correct_predictions / total_samples
+print(f'Test Accuracy: {100 * test_accuracy:.2f}%')
+
+torch.save(model.state_dict(), 'model_vehicles.pth')
+
+class_names_file = "class_names.txt"
+
+with open(class_names_file, "w") as file:
+    for class_name in class_names:
+        file.write(class_name + "\n")
